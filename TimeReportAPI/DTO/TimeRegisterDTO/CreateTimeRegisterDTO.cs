af8bdd28bfe4109329ce4c3d2cf9dbfd0c5462bb
Gong-Code <gong.moonphruk@gmail.com>
2022-05-21 17:04:11 +0200

    aaa

--- TimeReportAPI/DTO/TimeRegisterDTO/CreateTimeRegisterDTO.cs
+++ TimeReportAPI/DTO/TimeRegisterDTO/CreateTimeRegisterDTO.cs
@@ -3,11 +3,9 @@
 namespace TimeReportAPI.DTO.TimeRegisterDTO
 {
     public class CreateTimeRegisterDTO
-    {
-        
+    {  
         public int Hours { get; set; }
 
-       
         public int Minutes { get; set; }
 
         public int ProjectId { get; set; }
